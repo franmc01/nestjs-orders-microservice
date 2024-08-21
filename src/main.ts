55b8f91cb15fb26e3dabc9b58b84442e236b6606
t1fmarin <fmarin@iuvity.com>
2024-08-21 12:28:30 -0500

    feat: configure initial files

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { envs } from './config/envs';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const logger = new Logger();
+
+  const app = await NestFactory.create(AppModule);
+  await app.listen(envs.port);
+
+  logger.log(`Orders microservice running on port ${envs.port}`);
+}
+bootstrap();
